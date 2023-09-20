598bcd53c056f47c7d3092b1465c6cd3f4aacf83
matt <matthewarcherr@gmail.com>
2023-09-20 20:49:25 +0100

    set up user session username

--- server-cli/server.js
+++ server-cli/server.js
@@ -7,22 +7,30 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 var log = [];
+const usernameMap = new Map();
 
 app.get('/', (req, res) => {
   res.send('<script src="/socket.io/socket.io.js"></script><script>var socket = io();</script>');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  var userName;
+
+  socket.on('name', (msg) => {
+    userName = msg;
+    console.log(userName + ' connected');
+    log.push(userName + ' connected');
+  });
 
   socket.on('msg', (msg) => {
-    console.log(msg['name']+ ": "+msg['msg']);
+    console.log(userName+": "+msg['msg']);
 
-    log.push(msg['name']+ ": "+msg['msg'])
+    log.push(userName+": "+msg['msg'])
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log(userName + ' disconnected');
+    log.push(userName + ' disconnected');
   });
 });
 
