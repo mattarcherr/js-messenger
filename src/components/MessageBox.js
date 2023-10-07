24dd879f3429f939a599be0b94099be3ac47fe79
matt <matthewarcherr@gmail.com>
2023-10-07 20:03:39 +0100

    messagebox css

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -5,17 +5,19 @@ export default function MessageBox() {
 
     const handleSend = (e) => {
         e.preventDefault();
-        const msg = document.getElementById("message")
+        const msg = document.getElementById("messageInput")
+
+        if (msg.value === '') return;
         socket.emit("msg", msg.value);
         msg.value = "";
     }
 
     return (
     <div id='MessageBox-div'>
-        <form>
-            <input type="text" id="message"/>
+        <form id='submitForm'>
+            <input type="text" id="messageInput"/>
 
-            <input type="submit" value="send"
+            <input type="submit" value="send" id='sendButton'
              onClick={handleSend} 
             />
         </form>
